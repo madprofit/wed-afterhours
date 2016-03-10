132db8a04b1d40edc3e238f0e219bcc99b63e9e4
Carlos Velasquez <cv.velasquez@gmail.com>
2016-03-09 21:34:31 -0800

    loops

---
+++ app.js
@@ -0,0 +1,13 @@
+
+var classmates = ["erick", "lisa", "Shon", "Sing", "david"];
+
+for(var i = 0; i < classmates.length; i++) {
+  console.log(classmates[i]);
+}
+
+var x = 0;
+var students = ["kate", "phil", "matt", "jenny", "carlos"];
+while(x < students.length){
+  console.log(students[x]);
+  x++;
+}
